00ddc2d080948872d2a822de14cc752018e18e04
JoTerrance <gorka.zgz@gmail.com>
2019-12-19 09:18:20 +0100

    created initial app

--- myproject/force-app/main/default/aura/myComponent/myComponentHelper.js
+++ myproject/force-app/main/default/aura/myComponent/myComponentHelper.js
@@ -1,6 +1,27 @@
 ({
     helperMethod : function(component, event) {
         var textodelusuario = component.find('entradauusario').getelement().value;
-        
+        component.set("v.myvariable","valordelavariable");
+      
+        var action =component.get("c.conparam");
+        action.setparams({
+            hola:"buenosdias"           
+        });
+
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state==="SUCCESS"){
+                component.set("v.data",response.getReturnValue());
+            }
+            if(state==="INCOMPLETE"){}
+            if(state==="ERROR"){
+                var errors = response.getError();
+                if(errors){
+                    if(errors[0] && errors[0].message ){
+                        console.log(errors[0].message);
+                    }
+                }
+            }
+        })
     }
 })

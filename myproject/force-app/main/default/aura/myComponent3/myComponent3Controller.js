a6fcc66d9efb6ef25b7f9d9db0e5e720fe36cada
JuanPardos <juanpavh97@gmail.com>
2020-02-26 16:35:23 +0100

    More changes.
    Signed-off-by: JuanPardos <juanpavh97@gmail.com>

--- myproject/force-app/main/default/aura/myComponent3/myComponent3Controller.js
+++ myproject/force-app/main/default/aura/myComponent3/myComponent3Controller.js
@@ -2,15 +2,23 @@
     saveData: function(component, event, helper) {
         var inputName = component.find('inputName').getElement().value;
         var inputYear = component.find('inputYear').getElement().value;
+        var inputCapital = component.find('inputCapital').getElement().value;
+        var inputRuinas = component.find('inputRuinas').getElement().value;
 
         var list = component.get("v.aux");
         var aux = component.get("v.list");
-  
+        var countID = component.get("v.count");
+
         aux.push({
+            ID: countID,
             name: inputName,
             year: inputYear,
+            capital: inputCapital,
+            ruinas: inputRuinas,
         });
 
+        countID++;
+        component.set("v.count", countID);
         component.set("v.list", aux); 
     },
 
--- myproject/force-app/main/default/aura/myComponent4/myComponent4Controller.js
+++ myproject/force-app/main/default/aura/myComponent4/myComponent4Controller.js
@@ -5,5 +5,32 @@
 
     onChildAttributeChange2 : function (component, event, helper) {
         
+    },
+
+    callAura : function(component, event) {
+        var action =component.get("c.callWithParam");
+        action.setParams({
+            msg:"buenosdias"           
+        });
+
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state==="SUCCESS"){
+                component.set("v.myvariable",userInput+response.getReturnValue());                
+            }
+            if(state==="INCOMPLETE"){
+            }
+            if(state==="ERROR"){
+                var errors = response.getError();
+                if(errors){
+                    if(errors[0] && errors[0].message ){
+                        console.log(errors[0].message);
+                    }
+                    component.set("v.myvariable","error");
+                }
+            }
+
+        });
+        $A.enqueueAction(action);
     }
 })
